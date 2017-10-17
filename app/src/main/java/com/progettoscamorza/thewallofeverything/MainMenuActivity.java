0d8ca61de01549a7f2f8c3f0f4ba0217559a11d0
Luca <luca.gugole99@gmail.com>
2017-10-17 15:57:18 +0200

    Aggiunta menuActivity e startButton

---
+++ app/src/main/java/com/progettoscamorza/thewallofeverything/MainMenuActivity.java
@@ -0,0 +1,14 @@
+package com.progettoscamorza.thewallofeverything;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class MainMenuActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
