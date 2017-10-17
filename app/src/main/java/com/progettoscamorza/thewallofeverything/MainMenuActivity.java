9f7eae7cf8a133ce3690442551f0a291d59a5064
enric <enricovrz2811@gmail.com>
2017-10-17 17:56:29 +0200

    aggiunta 3 +1 button seconda schermata

--- app/src/main/java/com/progettoscamorza/thewallofeverything/MainMenuActivity.java
+++ app/src/main/java/com/progettoscamorza/thewallofeverything/MainMenuActivity.java
@@ -9,6 +9,7 @@ public class MainMenuActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);
 
     }
 }
