68879f807512a54062d13e836a70ffe06f3f807a
Luca <luca.gugole99@gmail.com>
2017-10-31 14:15:33 +0100

    creazione nuova activity

---
+++ app/src/main/java/com/progettoscamorza/thewallofeverything/activities/SettingsActivity.java
@@ -0,0 +1,10 @@
+package com.progettoscamorza.thewallofeverything.activities;
+
+import android.app.Activity;
+
+/**
+ * Created by Luca on 31/10/2017.
+ */
+
+public class SettingsActivity extends Activity {
+}
