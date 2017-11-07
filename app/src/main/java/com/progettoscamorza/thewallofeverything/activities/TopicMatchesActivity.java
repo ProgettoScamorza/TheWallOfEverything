04cc896869ca68721df72d93705fa456081d688e
Luca <luca.gugole99@gmail.com>
2017-11-07 14:30:42 +0100

    creata activity topicMaches e modifica string.xml

---
+++ app/src/main/java/com/progettoscamorza/thewallofeverything/activities/TopicMatchesActivity.java
@@ -0,0 +1,17 @@
+package com.progettoscamorza.thewallofeverything.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.progettoscamorza.thewallofeverything.R;
+
+
+public class TopicMatchesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_topic_matches);
+    }
+}
