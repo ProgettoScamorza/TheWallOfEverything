0496970323aad614ab1908b657f57f6b4b307708
Mucca_Lunare <angero.riccardo@gmail.com>
2017-11-07 15:04:36 +0100

    Colorato i bottoni e aggiunto i bottoni alla classe activity topics. Ho anche sostituito il bottone return con la freccia nelle classi.

--- app/src/main/java/com/progettoscamorza/thewallofeverything/activities/MainActivity.java
+++ app/src/main/java/com/progettoscamorza/thewallofeverything/activities/MainActivity.java
@@ -21,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), MainMenuActivity.class);
                 startActivity(intent);
+
+
             }
         });
 
