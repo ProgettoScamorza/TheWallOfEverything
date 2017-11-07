12b4ef7208bbc122bcc2089e328177cddac845d8
Petru <petreabaltag@gmail.com>
2017-11-07 15:19:49 +0100

    Ottimizzazione TopicsActivity e Implementazine di 2 tasti con intent

--- app/src/main/java/com/progettoscamorza/thewallofeverything/activities/MainMenuActivity.java
+++ app/src/main/java/com/progettoscamorza/thewallofeverything/activities/MainMenuActivity.java
@@ -1,8 +1,11 @@
 package com.progettoscamorza.thewallofeverything.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
 
 import com.progettoscamorza.thewallofeverything.R;
 
@@ -13,5 +16,14 @@ public class MainMenuActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
 
+        Button topicsButton = (Button) findViewById(R.id.button3);
+        topicsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),TopicsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
