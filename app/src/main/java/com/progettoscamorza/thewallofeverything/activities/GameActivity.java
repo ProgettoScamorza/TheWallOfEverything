4b835a62370f9686c51765906a25e9507f0844be
Luca <luca.gugole99@gmail.com>
2017-12-05 16:10:44 +0100

    inizio grafica 3d e camabio API richieste da 15 a 18.

--- app/src/main/java/com/progettoscamorza/thewallofeverything/activities/GameActivity.java
+++ app/src/main/java/com/progettoscamorza/thewallofeverything/activities/GameActivity.java
@@ -2,10 +2,11 @@ package com.progettoscamorza.thewallofeverything.activities;
 
 import android.app.Activity;
 import android.arch.persistence.room.Room;
-import android.content.Intent;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 
 import com.progettoscamorza.thewallofeverything.R;
+import com.progettoscamorza.thewallofeverything.graphics.GameUI;
 import com.progettoscamorza.thewallofeverything.persistence.DatabaseManager;
 
 public class GameActivity extends Activity {
@@ -20,5 +21,12 @@ public class GameActivity extends Activity {
         //database instance
         databaseManager = Room.databaseBuilder(
                 getApplicationContext(), DatabaseManager.class, "WOE-DB").build();
+
+        loadGraphic();
+    }
+
+    private void loadGraphic() {
+        GLSurfaceView glSurfaceView = new GLSurfaceView(getApplicationContext());
+        glSurfaceView.setRenderer(new GameUI());
     }
 }
