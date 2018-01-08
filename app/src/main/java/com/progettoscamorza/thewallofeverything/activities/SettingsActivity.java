75db50add55c336da6ea5c1ca066277b22385f46
enric <enricovrz2811@gmail.com>
2018-01-08 19:00:04 +0100

    logo app

--- app/src/main/java/com/progettoscamorza/thewallofeverything/activities/SettingsActivity.java
+++ app/src/main/java/com/progettoscamorza/thewallofeverything/activities/SettingsActivity.java
@@ -2,6 +2,7 @@ package com.progettoscamorza.thewallofeverything.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Switch;
 
 import com.progettoscamorza.thewallofeverything.R;
 
@@ -11,6 +12,14 @@ public class SettingsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Switch soundSwitch = (Switch)findViewById(R.id.switch1);
+        String statusSwitch1;
+        if (soundSwitch.isChecked())
+            statusSwitch1 = soundSwitch.getTextOn().toString();
+        else
+            statusSwitch1 = soundSwitch.getTextOff().toString();
+
     }
 
 }
